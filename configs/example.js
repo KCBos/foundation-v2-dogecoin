ee84491fe602374186d4fa407b166741bb7e013a
blinkhash <blinkhashpool@gmail.com>
2022-06-13 11:39:19 -0400

    Start building out Dogecoin compatibility

--- configs/example.js
+++ configs/example.js
@@ -1,6 +1,6 @@
 /*
  *
- * Example (Bitcoin)
+ * Example (Dogecoin)
  *
  */
 
@@ -49,15 +49,15 @@ config.primary.address = 'example';
 
 // Coin Configuration
 config.primary.coin = {};
-config.primary.coin.name = 'Bitcoin';
-config.primary.coin.symbol = 'BTC';
+config.primary.coin.name = 'Dogecoin';
+config.primary.coin.symbol = 'DOGE';
 
 // Daemon Configuration
 config.primary.daemons = [];
 
 const daemons1 = {};
 daemons1.host = '127.0.0.1';
-daemons1.port = 8332;
+daemons1.port = 22555;
 daemons1.username = '';
 daemons1.password = '';
 config.primary.daemons.push(daemons1);
